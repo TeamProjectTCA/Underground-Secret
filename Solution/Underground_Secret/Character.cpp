9e8d55cbb62ea5546d55d91818f06cb6f85a8f31
tika <a>
2018-05-04 17:53:12 +0900

    characterクラス拡張 アニメーションがセットできるようにした

--- Solution/Underground_Secret/Character.cpp
+++ Solution/Underground_Secret/Character.cpp
@@ -1,7 +1,63 @@
 #include "Character.h"
+#include "Drawer.h"
+#include <assert.h>
+#include <errno.h>
 
-Character::Character( ) {
+const int DEFAULT_ANIM_TIME = 30;
+
+Character::Character( std::string walk_anim_filename, int walk_anim_frame_num ) {
+	_drawer = Drawer::getTask( );
+	addAnim( WALK, walk_anim_filename, walk_anim_frame_num );
+
+	_anim_type = WALK;
+	_anim_change_time = DEFAULT_ANIM_TIME;
+
+	_cnt = 0;
+	_pos = Vector( );
 }
 
 Character::~Character( ) {
+}
+
+void Character::addAnim( ANIM_TYPE type, std::string filename, int frame_num ) {
+	int handle = _drawer->getImage( filename );
+	errno_t addAnim = handle;
+	assert( addAnim != -1 );
+
+	_anim[ type ].handle = handle;
+	_anim[ type ].frame = frame_num;
+	_anim[ type ].height = _drawer->getImageHeight( filename );
+	_anim[ type ].width = _drawer->getImageWidth( filename ) / frame_num;
+}
+
+void Character::setAnim( ANIM_TYPE type ) {
+	if ( _anim.find( type ) == _anim.end( ) ) {
+		return;
+	}
+
+	_anim_type = type;
+	_max_cnt = _anim[ _anim_type ].frame * _anim_change_time;
+	_cnt = 0;
+}
+
+void Character::setAnimTime( int change_time ) {
+	_anim_change_time = change_time;
+	_max_cnt = _anim[ _anim_type ].frame * _anim_change_time;
+}
+
+void Character::draw( ) {
+	_cnt = ( _cnt + 1 ) % _max_cnt;
+
+	int sx = 0;
+	if ( _cnt % _anim_change_time == 0 ) {
+		// ‰æ‘œ‚ðØ‚è‘Ö‚¦‚é( ¶’[‚ð‚¸‚ç‚· )
+		sx = ( ( _cnt / _anim_change_time ) % _anim[ _anim_type ].frame ) * _anim[ _anim_type ].width;
+	}
+
+	_drawer->drawRectGraph( 
+		( float )_pos.x, ( float )_pos.y,
+		sx, 0,
+		_anim[ _anim_type ].width, _anim[ _anim_type ].height, 
+		_anim[ _anim_type ].handle,
+		true, false );
 }
--- Solution/Underground_Secret/Character.h
+++ Solution/Underground_Secret/Character.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "smart_ptr.h"
+#include "Vector.h"
+#include <string>
+#include <map>
+#include <vector>
 
 PTR( Character );
+PTR( Drawer );
 
 enum CHARACTER {
 	CHARA_A,
@@ -10,9 +15,56 @@ enum CHARACTER {
 
 class Character {
 public:
-	Character( );
+	enum ANIM_TYPE {
+		WALK,
+	};
+
+private:
+	struct Animation {
+		int handle;
+		int frame;
+		int width;
+		int height;
+
+		Animation( ) :
+			handle( -1 ),
+			frame( 1 ),
+			width( 1 ),
+			height( 1 ) {
+		};
+
+		Animation( int input_handle, int input_frame, int input_width, int input_height ) :
+			handle( input_handle ),
+			frame( input_frame ),
+			width( input_width ),
+			height( input_height ) {
+		};
+	};
+
+public:
+	Character( std::string walk_anim_filename, int walk_anim_frame_num );
 	virtual ~Character( );
 
 public:
+	// 新しくAnimationを追加
+	void addAnim( ANIM_TYPE type, std::string filename, int frame_num );
+	// アニメーションを切り替える
+	void setAnim( ANIM_TYPE type );
+	void setAnimTime( int change_time );
+
+public:
+	void draw( );
 	virtual void update( ) = 0;
+
+protected:
+	Vector _pos;
+
+private:
+	std::map< ANIM_TYPE, Animation > _anim;
+	int _anim_change_time;
+	int _max_cnt;
+	int _cnt;
+	ANIM_TYPE _anim_type;
+
+	DrawerPtr _drawer;
 };
